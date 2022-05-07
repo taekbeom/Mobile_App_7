115a8244d3ba363043de0eccd191abd1915c028e
taekbeom <oumamarrysaihara@gmail.com>
2022-05-07 19:43:26 +0300

    Initial commit

---
+++ httpurlconnection/src/main/java/ru/mirea/lebedeva/httpurlconnection/MainActivity.java
@@ -0,0 +1,134 @@
+package ru.mirea.lebedeva.httpurlconnection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView ipTextView;
+    private TextView countryTextView;
+    private TextView regionTextView;
+    private TextView regCodeTextView;
+
+    private String url = "https://ip.seeip.org/geoip";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ipTextView = findViewById(R.id.ipTextView);
+        countryTextView = findViewById(R.id.countryTextView);
+        regionTextView = findViewById(R.id.regionTextView);
+        regCodeTextView = findViewById(R.id.regCodeTextView);
+    }
+
+    public void onClick(View view) {
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkinfo = null;
+        if (connectivityManager != null) {
+            networkinfo = connectivityManager.getActiveNetworkInfo();
+        }
+        if (networkinfo != null && networkinfo.isConnected()) {
+            new DownloadPageTask().execute(url);
+        } else {
+            Toast.makeText(this, "Нет интернета", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private class DownloadPageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadIpInfo(urls[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "error";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d(MainActivity.class.getSimpleName(), result);
+            try {
+                JSONObject responseJson = new JSONObject(result);
+                String ip = responseJson.getString("ip");
+                String country = responseJson.getString("country");
+                String region = responseJson.getString("region");
+                String regCode = responseJson.getString("region_code");
+                ipTextView.setText("Ip: " + ip);
+                countryTextView.setText("Country: " + country);
+                regionTextView.setText("Region: " + region);
+                regCodeTextView.setText("Region code: " + regCode);
+                Log.d(MainActivity.class.getSimpleName(), ip);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            super.onPostExecute(result);
+        }
+    }
+
+    private String downloadIpInfo(String address) throws IOException {
+        InputStream inputStream = null;
+        String data = "";
+        try {
+            URL url = new URL(address);
+            HttpURLConnection connection = (HttpURLConnection) url
+                    .openConnection();
+            connection.setReadTimeout(100000);
+            connection.setConnectTimeout(100000);
+            connection.setRequestMethod("GET");
+            connection.setInstanceFollowRedirects(true);
+            connection.setUseCaches(false);
+            connection.setDoInput(true);
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                inputStream = connection.getInputStream();
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                int read = 0;
+                while ((read = inputStream.read()) != -1) {
+                    bos.write(read);
+                }
+                byte[] result = bos.toByteArray();
+                bos.close();
+                data = new String(result);
+            } else {
+                data = connection.getResponseMessage() + " . Error Code : " + responseCode;
+            }
+            connection.disconnect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return data;
+    }
+}
